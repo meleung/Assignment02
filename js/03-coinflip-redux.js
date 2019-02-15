12c248bd353fd2c35df002f778c727dca9be4c29
Matthew Leung <matthew.e.leung@gmail.com>
2019-02-14 18:15:44 -0800

    Complete Coin Flip Reduce
    Complete the Coin Flip Reduce section of the assignment.

---
+++ js/03-coinflip-redux.js
@@ -0,0 +1,12 @@
+/*eslint-env browser*/
+var coinFlip;
+var i;
+
+for (i = 0; i < 10; i++) {
+    coinFlip = Math.round(Math.random());
+    if (coinFlip === 0) {
+        console.log("Heads");
+    } else {
+        console.log("Tails");
+    }
+}
