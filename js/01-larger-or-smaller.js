b526925b19a74febdf017f0a3a3dea99ca9cdb4b
Matthew Leung <matthew.e.leung@gmail.com>
2019-02-14 17:50:38 -0800

    Complete Larger or Smaller
    Completed Larger or Smaller section of the assignment

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,1 +1,9 @@
 /*eslint-env browser*/
+var num1, num2, output;
+
+num1 = parseInt(window.prompt("Enter the first number:"));
+num2 = parseInt(window.prompt("Enter the second number:"));
+
+output = num1 > num2 ? num1 : num2;
+
+window.document.write(output);
